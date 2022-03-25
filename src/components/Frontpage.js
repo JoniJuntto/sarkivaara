b620eadff23a5038c08464f3d83ba052125e9ee1
Joni Juntto <joni.juntto@mustigroup.com>
2022-03-25 17:09:54 +0200

    Test

--- src/components/Frontpage.js
+++ src/components/Frontpage.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Typography, Paper } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import mursuEka from '../pictures/mursu4.jpg';
 import pentu from '../pictures/pentu.jpg';
 import Intro from './tekstit/Intro';
 import Pentu from './tekstit/Pentu';
-import Testi from './tekstit/Testi';
 import kiukku from '../pictures/kiukku2.jpg';
+import Yhteystiedot from './tekstit/Yhteystiedot';
 
 const useStyles = makeStyles((theme) => ({
 
@@ -120,9 +119,10 @@ export default function App(){
             </div>
             <div className={classes.div3}>
                 <div className={classes.test}>
-                    <Testi />
+                    <Yhteystiedot />
                 </div>
             </div>
         </div>
     );
 }
+
